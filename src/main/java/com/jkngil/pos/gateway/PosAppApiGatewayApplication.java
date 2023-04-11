b37c14c41cc8013a86d02bcbe535ce6e3dc4767c
kevsoriano <kevinsoriano173@gmail.com>
2023-04-11 18:11:57 +0800

    added routes

---
+++ src/main/java/com/jkngil/pos/gateway/PosAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jkngil.pos.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAppApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAppApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkngil/pos/gateway/PosAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkngil.pos.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAppApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
